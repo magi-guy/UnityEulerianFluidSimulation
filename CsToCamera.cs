4ac464c34b6f788657138c5583c5317102bc9682
MagiGuy <jdjamrosz@stu.naperville203.org>
2023-10-30 09:33:56 -0500

    Test Commit

--- CsToCamera.cs
+++ CsToCamera.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CsToCamera : MonoBehaviour
 {
+    [SerializeField] private int compute;
     // Start is called before the first frame update
     void Start()
     {

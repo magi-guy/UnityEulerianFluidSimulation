eeed347ea5cb32a9852c1617ac35182c7556e743
MagiGuy <111530308+magi-guy@users.noreply.github.com>
2023-11-01 13:36:30 -0500

    Everything works
    As long as I don't use my chromebook

--- CsToCamera.cs
+++ CsToCamera.cs
@@ -10,10 +10,11 @@ public class CsToCamera : MonoBehaviour
 
     public RenderTexture texture;
     public ComputeBuffer buffer;
+    private float time;
 
     void Start()
     {
-        texture = new RenderTexture(width, height, 24);
+        texture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
         texture.enableRandomWrite = true;
         texture.Create();
 
@@ -24,20 +25,22 @@ public class CsToCamera : MonoBehaviour
         compute.Dispatch(kernel, width/8, height/8, 1);
     }
 
-    // void OnRenderImage(RenderTexture src, RenderTexture dest)
-    // {
-    //     if(texture == null) {
-    //         texture = new RenderTexture(width, height, 24);
-    //         texture.enableRandomWrite = true;
-    //         texture.Create();
-    //     }
-    //     compute.SetTexture(0, "Result", texture);
-    //     compute.SetFloat("width", width);
-    //     compute.SetFloat("height", height);
-    //     compute.Dispatch(0, width/8, height/8, 1);
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if(texture == null) {
+            texture = new RenderTexture(width, height, 24);
+            texture.enableRandomWrite = true;
+            texture.Create();
+        }
+        compute.SetTexture(0, "Result", texture);
+        compute.SetFloat("width", width);
+        compute.SetFloat("height", height);
+	time += Time.deltaTime; Debug.Log(time);
+	compute.SetFloat("time", time);
+        compute.Dispatch(0, width/8, height/8, 1);
 
-    //     Graphics.Blit(texture, dest);
-    // }
+        Graphics.Blit(texture, dest);
+    }
 
     void Update()
     {
